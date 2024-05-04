c2d382248e37d1992f19ddb08ee9ee8a9b607f6e
Abhishek Sunkarwad <abhisunkarwad2002@gmail.com>
2024-05-04 22:50:41 +0530

    created api to fetch jobs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,19 @@
 import {React,useState,useEffect} from 'react'
 import { Filters } from './Filters'
+import {fetchJobData} from '../api/JobService'
 
 export function Home(){
+  const [jobData, setJobData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchJobData();
+      setJobData(data);
+      console.log(jobData);
+    };
+    fetchData();
+  }, []);
+  
   return (
     <div>
          <Filters/>
